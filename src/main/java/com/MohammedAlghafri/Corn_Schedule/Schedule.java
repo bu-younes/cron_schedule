55f9e16e7093141cb83b96b693b26121af95342a
bu-younes <alg7afri.m@gmail.com>
2023-05-31 11:39:38 +0400

    answer Q15

--- src/main/java/com/MohammedAlghafri/Corn_Schedule/Schedule.java
+++ src/main/java/com/MohammedAlghafri/Corn_Schedule/Schedule.java
@@ -50,6 +50,7 @@ public class Schedule {
     @Scheduled(cron = "0 0/5 * * * MON-FRI")
     @Scheduled(cron = "0 0/15 * * * SAT,SUN")
             //Problem 15: Write a CRON expression that runs a job every 10 minutes but only between 9:00 AM and 6:00 PM on weekdays.
+    @Scheduled(cron = "0 0/10 9-17 * * MON-FRI")
 
            // Problem 16: Write a CRON expression that runs a job every hour during the first half of the month.
 
